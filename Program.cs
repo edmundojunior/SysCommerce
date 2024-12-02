3a93752ce90509627cf08e5d78305936fdaf6a1b
Edmundo <edmundo.junior@nscinfo.com.br>
2024-12-02 20:33:25 -0300

    Iniciando Projeto

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace SysCommerce
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmPrincipal());
         }
     }
 }
